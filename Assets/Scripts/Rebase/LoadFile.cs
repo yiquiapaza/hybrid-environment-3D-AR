968c1a0cf2bc6d24b3d2af9f888641bda3d440d4
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-05-27 01:15:13 -0300

    Load JSON data this is working sync

--- Assets/Scripts/Rebase/LoadFile.cs
+++ Assets/Scripts/Rebase/LoadFile.cs
@@ -6,39 +6,13 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEditor;
 
-#if WINDOWS_UWP
-    using Windows.Storage;
-    using Windows.Storage.Pickers;
-    using System.Threading.Tasks;
-#endif
-
-public class LoadFile : MonoBehaviour
+public class LoadFile:MonoBehaviour
 {
-
-    public string folder_name;
-
-    // Start is called before the first frame update
-    void Start()
+    private UtilIO IO;
+    private void Start()
     {
-        CreatFolder(folder_name);
+        IO = new UtilIO();
+        Debug.Log(IO.ReadFile("Data", "data.json", gameObject));
     }
 
-    private void CreatFolder(string folderName)
-    {
-
-#if UNITY_EDITOR
-        string path_test = AssetDatabase.CreateFolder("Assets", folderName);
-        Debug.Log(AssetDatabase.GUIDToAssetPath(path_test));
-#endif
-#if WINDOWS_UWP
-        Task directory = new Task(
-            async() => 
-            {
-                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-                StorageFolder newFolder = await localFolder.CreateFolderAsync(folderName, CreationCollisionOption.FailIfExists);
-            }
-        );
-        directory.Start();
-#endif
-    }
 }
---
+++ Assets/Visualizations/UtilIO.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+#if WINDOWS_UWP
+    using Windows.Storage;
+    using Windows.Storage.Pickers;
+    using System.Threading.Tasks;
+#endif
+
+public class UtilIO
+{
+    public void CreatFolder(string nameFolder)
+    {
+#if UNITY_EDITOR
+        if(AssetDatabase.IsValidFolder(nameFolder))
+        {
+            AssetDatabase.CreateFolder("Assets", nameFolder);
+            Debug.Log("Create folder Success");
+        }
+        
+#endif
+#if WINDOWS_UWP
+        /*
+        Task directory = new Task(
+            async() => 
+            {
+                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                
+                StorageFolder newFolder = await localFolder.CreateFolderAsync(folderName, CreationCollisionOption.FailIfExists);
+            }
+        );
+        directory.Start();
+        */
+#endif
+    }
+
+    public string ReadFile(string NameFolder, string NameFile, GameObject gameObject)
+    {
+        string jsonData = "";
+#if UNITY_EDITOR
+        CreatFolder(NameFolder);
+        Debug.Log(Directory.GetCurrentDirectory() + "\\Assets\\" + NameFolder + "\\" + NameFile);
+        if (File.Exists(Directory.GetCurrentDirectory() + "\\Assets\\" + NameFolder + "\\" + NameFile))
+        {
+            StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\Assets\\" + NameFolder + "\\" + NameFile);
+            jsonData = sr.ReadToEnd();
+        }
+#endif
+#if WINDOWS_UWP
+        jsonData = Path.Combine(ApplicationData.Current.LocalFolder.Path, NameFile);
+        if(string.IsNullOrEmpty(jsonData))
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        else
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        Debug.Log(jsonData);
+#endif
+
+        return jsonData;
+    }
+}
