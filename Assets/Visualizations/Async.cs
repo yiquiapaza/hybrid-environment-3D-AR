578a48716684731a2a0f6a7a886b6c6ed6b391c2
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-06-16 01:36:22 -0300

    Voice Control

---
+++ Assets/Visualizations/Async.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Visualizations/Async")]
+public class Async : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendData()
+    {
+        Debug.Log("Test");
+    }
+
+}
