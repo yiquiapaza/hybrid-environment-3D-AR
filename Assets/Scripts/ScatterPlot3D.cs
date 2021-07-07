3053522fcb680e935a77f186ae5d123d5a2c6852
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-07 19:34:45 -0300

    Scatter plot 3D update

--- Assets/Scripts/ScatterPlot3D.cs
+++ Assets/Scripts/ScatterPlot3D.cs
@@ -9,33 +9,49 @@ public class ScatterPlot3D : MonoBehaviour
     [SerializeField]
     private GameObject TemplateObject = null;
 
-    [Tooltip("Material that use for regin 1")]
+    [Tooltip("Material that use for OC region")]
     [SerializeField]
-    private Material Region1 = null;
-    [Tooltip("Material that use for regin 2")]
+    private Material OC = null;
+    [Tooltip("Material that use for AS region")]
     [SerializeField]
-    private Material Region2 = null;
-    [Tooltip("Material that use for regin 3")]
+    private Material AS = null;
+    [Tooltip("Material that use for EU region")]
     [SerializeField]
-    private Material Region3 = null;
-    [Tooltip("Material that use for regin 4")]
+    private Material EU = null;
+    [Tooltip("Material that use for AF region")]
     [SerializeField]
-    private Material Region4 = null;
+    private Material AF = null;
+    [Tooltip("Material that use for NA region")]
+    [SerializeField]
+    private Material NA = null;
 
     private GameObject CountryA = null;
     private GameObject CountryB = null;
     private GameObject CountryC = null;
     private GameObject CountryD = null;
+    private GameObject CountryE = null;
+    private GameObject CountryF = null;
+    private GameObject CountryG = null;
+    private GameObject CountryH = null;
+    private GameObject CountryI = null;
+    private GameObject CountryJ = null;
+    private GameObject CountryK = null;
+    private GameObject CountryL = null;
+    private GameObject CountryM = null;
+    private GameObject CountryN = null;
+    private GameObject CountryO = null;
+    private GameObject CountryP = null;
 
     private string RawData = null;
     private GenericObjects DataObject = null;
+
+    private GameObject test;
 
     void Start()
     {
         GetData();
         InitialInstantiateObjects();
         StartCoroutine(WaitOneSecond());
-        UpdatePositionObjects();
     }
 
     // Update is called once per frame
@@ -56,6 +72,7 @@ public class ScatterPlot3D : MonoBehaviour
             }
             else
             {
+                UpdatePositionObjects();
                 Debug.Log(client.downloadHandler.data);
             }
         }
@@ -69,9 +86,9 @@ public class ScatterPlot3D : MonoBehaviour
             //LogToUnityConsole(i, flag);
             tmp++;
             if (tmp % 2 == 0)
-                CountryA.GetComponent<Renderer>().material = Region1;
+                CountryA.GetComponent<Renderer>().material = OC;
             else
-                CountryA.GetComponent<Renderer>().material = Region2;
+                CountryA.GetComponent<Renderer>().material = EU;
             yield return StartCoroutine(ObjectPositionUpdate());
         }
     }
@@ -100,30 +117,122 @@ public class ScatterPlot3D : MonoBehaviour
         CountryB = Instantiate(TemplateObject);
         CountryC = Instantiate(TemplateObject);
         CountryD = Instantiate(TemplateObject);
+        CountryE = Instantiate(TemplateObject);
+        CountryF = Instantiate(TemplateObject);
+        CountryG = Instantiate(TemplateObject);
+        CountryH = Instantiate(TemplateObject);
+        CountryI = Instantiate(TemplateObject);
+        CountryJ = Instantiate(TemplateObject);
+        CountryK = Instantiate(TemplateObject);
+        CountryL = Instantiate(TemplateObject);
+        CountryM = Instantiate(TemplateObject);
+        CountryN = Instantiate(TemplateObject);
+        CountryO = Instantiate(TemplateObject);
+        CountryP = Instantiate(TemplateObject);
 
-        CountryA.GetComponent<Renderer>().material = Region1;
-        CountryB.GetComponent<Renderer>().material = Region2;
-        CountryC.GetComponent<Renderer>().material = Region3;
-        CountryD.GetComponent<Renderer>().material = Region4;
+        test = Instantiate(TemplateObject);
+        test.GetComponent<Renderer>().material.color = Color.red;
+        test.transform.localPosition = gameObject.transform.position;
 
-        Debug.Log(CountryA.gameObject.name);
-        Debug.Log(CountryB.gameObject.name);
+        CountryA.GetComponent<Renderer>().material = OC;
+        CountryB.GetComponent<Renderer>().material = AS;
+        CountryC.GetComponent<Renderer>().material = EU;
+        CountryD.GetComponent<Renderer>().material = AF;
+        CountryE.GetComponent<Renderer>().material = EU;
+        CountryF.GetComponent<Renderer>().material = AS;
+        CountryG.GetComponent<Renderer>().material = AS;
+        CountryH.GetComponent<Renderer>().material = AF;
+        CountryI.GetComponent<Renderer>().material = AF;
+        CountryJ.GetComponent<Renderer>().material = EU;
+        CountryK.GetComponent<Renderer>().material = OC;
+        CountryL.GetComponent<Renderer>().material = AF;
+        CountryM.GetComponent<Renderer>().material = AS;
+        CountryN.GetComponent<Renderer>().material = EU;
+        CountryO.GetComponent<Renderer>().material = NA;
+        CountryP.GetComponent<Renderer>().material = AF;
     }
 
     void UpdatePositionObjects(int year = 0, int axesX = 1, int axesY = 2, int axesZ = 3)
     {
         CountryA.transform.localPosition = new Vector3(
-            Util.ScaleEnergyConsumption(DataObject.countries[0].year[0].energy_consumption),
-            Util.ScaleGDPPercapita(DataObject.countries[0].year[0].gdp_per_capita), 0);
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[0].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[0].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
         CountryB.transform.localPosition = new Vector3(
-            Util.ScaleEnergyConsumption(DataObject.countries[1].year[0].energy_consumption),
-            Util.ScaleGDPPercapita(DataObject.countries[1].year[0].gdp_per_capita), 0);
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[1].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[1].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
         CountryC.transform.localPosition = new Vector3(
-            Util.ScaleEnergyConsumption(DataObject.countries[2].year[0].energy_consumption),
-            Util.ScaleGDPPercapita(DataObject.countries[2].year[0].gdp_per_capita), 0);
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[2].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[2].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
         CountryD.transform.localPosition = new Vector3(
-            Util.ScaleEnergyConsumption(DataObject.countries[3].year[0].energy_consumption),
-            Util.ScaleGDPPercapita(DataObject.countries[3].year[0].gdp_per_capita), 0);
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[3].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[3].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryE.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[4].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[4].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryF.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[5].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[5].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryG.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[6].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[6].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryH.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[7].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[7].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryI.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[8].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[8].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryJ.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[9].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[9].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryK.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[10].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[10].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryL.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[11].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[11].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryM.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[12].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[12].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryN.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[13].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[13].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryO.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[14].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[14].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
+
+        CountryP.transform.localPosition = new Vector3(
+            gameObject.transform.position.x + Util.ScaleEnergyConsumption(DataObject.countries[15].year[0].energy_consumption),
+            gameObject.transform.position.y + Util.ScaleGDPPercapita(DataObject.countries[15].year[0].gdp_per_capita),
+            gameObject.transform.position.z);
     }
 
     void GetData()
