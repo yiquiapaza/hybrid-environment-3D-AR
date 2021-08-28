9554bdbe454847c003f0009e09c92f6286e601e3
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-08-28 01:00:39 -0300

    Add message script

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Message : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject display;
+    [SerializeField]
+    private TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        display.GetComponent<Renderer>().material.color = Color.red;
+        text.text = "Preuba";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
