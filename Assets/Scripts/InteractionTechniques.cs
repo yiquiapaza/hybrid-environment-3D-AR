c73c6e57d00e1266b6ee08561d112f45c95996c5
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-10 02:36:25 -0300

    Try to add post method but have a lot of errors

--- Assets/Scripts/InteractionTechniques.cs
+++ Assets/Scripts/InteractionTechniques.cs
@@ -6,32 +6,59 @@ using UnityEngine.Networking;
 public class InteractionTechniques : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+
     private Vector3 globalCoordinates = Vector3.zero;
+
+    private WWWForm rotationPost;
+
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
 
     void Start()
     {
-        globalCoordinates.x = gameObject.transform.localPosition.x;
-        globalCoordinates.y = gameObject.transform.localPosition.y;
-        globalCoordinates.z = gameObject.transform.localPosition.z;
+        rotationPost = new WWWForm();
+        
+
+        globalCoordinates.x = gameObject.transform.rotation.x;
+        globalCoordinates.y = gameObject.transform.rotation.y;
+        globalCoordinates.z = gameObject.transform.rotation.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        StartCoroutine(SendRotation());
     }
 
     //TODO: implmentes these methods
     // Translation: move objects in both environment, send with the same data
     // Grab: grab objects in both enviroment with the same data
     // Rotation: rotation objects in the both environment with the same data.
-    
+
     void GlobalRotation()
     {
-        using(UnityWebRequest client = UnityWebRequest.Get("example")
-        {
+        Debug.Log(gameObject.transform.rotation);
+        new WaitForSeconds(1);
 
+    }
+
+    IEnumerator SendRotation()
+    {
+        rotationPost.AddField("x", gameObject.transform.rotation.x.ToString());
+        rotationPost.AddField("y", gameObject.transform.rotation.y.ToString());
+        rotationPost.AddField("z", gameObject.transform.rotation.z.ToString());
+        using (UnityWebRequest client = UnityWebRequest.Post("http://localhost/roration", rotationPost)) 
+        {
+            yield return client.SendWebRequest();
+
+            if (client.isHttpError || client.isHttpError)
+            {
+                Debug.Log(client.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
         }
     }
 }
