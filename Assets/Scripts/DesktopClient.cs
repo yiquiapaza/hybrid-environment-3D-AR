3ad0b126a127adaf4826ec6950f350855e34a2dc
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-17 23:43:41 -0300

    Refactoring desktopclient

--- Assets/Scripts/DesktopClient.cs
+++ Assets/Scripts/DesktopClient.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DesktopClient : MonoBehaviour
 {
+    private Vector3 globalRotation = Vector3.zero;
+
+    private WWWForm postRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class DesktopClient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Send the rotatio for sec
+    IEnumerator SendRotation()
+    {
+        postRotation = new WWWForm();
+
+        postRotation.AddField("x", gameObject.transform.rotation.x.ToString());
+        postRotation.AddField("y", gameObject.transform.rotation.y.ToString());
+        postRotation.AddField("z", gameObject.transform.rotation.z.ToString());
+        using UnityWebRequest client = UnityWebRequest.Post("http://192.168.0.104:3000/rotation", postRotation);
+        yield return client.SendWebRequest();
+
+        if (client.isHttpError || client.isHttpError)
+        {
+            Debug.Log(client.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
     }
 }
