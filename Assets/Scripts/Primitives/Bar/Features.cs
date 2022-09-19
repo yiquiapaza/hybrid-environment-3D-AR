8d65928f475a19951e5480ca89e665fd3c1205f5
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-09-19 11:00:33 -0300

    feat: create bar prefab

---
+++ Assets/Scripts/Primitives/Bar/Features.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Features : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private Vector3 InitialScale;
+    private Vector3 InitialBoxCollider;
+
+    Vector3 TempScale;
+    Vector3 TempCollider;
+
+    void Start()
+    {
+        InitialScale = transform.localScale;
+        InitialBoxCollider = GetComponent<BoxCollider>().size; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        if (!InitialScale.Equals(TempScale))
+        {
+            UpdateScale();
+        }
+        if (!InitialBoxCollider.Equals(TempCollider))
+        {
+            UpdateCollider();
+        }
+    }
+
+    private void UpdateScale()
+    {
+        TempScale = InitialScale;
+        transform.localScale = InitialScale;    
+    }
+
+    private void UpdateCollider()
+    {
+        TempCollider = InitialBoxCollider;
+        GetComponent<BoxCollider>().size = InitialScale;
+    }
+}
