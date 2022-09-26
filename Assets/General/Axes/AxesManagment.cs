1a28c7116c3b704b186d3b5333468ea394dc1201
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-09-26 11:52:00 -0300

    feat: using SimpleJSON and axes prefab

--- Assets/General/Axes/AxesManagment.cs
+++ Assets/General/Axes/AxesManagment.cs
@@ -12,7 +12,7 @@ public class AxesManagment : MonoBehaviour
     [SerializeField]
     private bool _activeX = false;
     [SerializeField]
-    private bool _swapped = false;
+    private bool _swappedX = false;
 
     [SerializeField]
     private bool _flipY;
@@ -20,16 +20,21 @@ public class AxesManagment : MonoBehaviour
     private string _labelY;
     [SerializeField]
     private bool _activeY;
-
+    [SerializeField]
+    private bool _swappedY = false;
 
     [SerializeField]
     private bool _flipZ;
     [SerializeField]
-    private string _labeZ;
+    private string _labelZ;
     [SerializeField]
     private bool _activeZ;
+    [SerializeField]
+    private bool _swappedZ = false;
 
     private GameObject AxesX;
+    private GameObject AxesY;
+    private GameObject AxesZ;
 
     // Start is called before the first frame update
     void Start()
@@ -47,9 +52,23 @@ public class AxesManagment : MonoBehaviour
     {
         AxesX = gameObject.transform.GetChild(0).gameObject;
         AxesX.GetComponent<GenericAxisPrefab>()._nameAxisLabel = _labeX;
-        AxesX.GetComponent<GenericAxisPrefab>()._swapped = _swapped;
+        AxesX.GetComponent<GenericAxisPrefab>()._swapped = _swappedX;
         AxesX.SetActive(_activeX);
         if(_flipX)
-            AxesX.transform.eulerAngles = new Vector3(AxesX.transform.rotation.y, 180f, AxesX.transform.rotation.z);
+            AxesX.transform.eulerAngles = new Vector3(AxesX.transform.rotation.x, 180f, AxesX.transform.rotation.z);
+
+        AxesY = gameObject.transform.GetChild(1).gameObject;
+        AxesY.GetComponent<GenericAxisPrefab>()._nameAxisLabel = _labelY;
+        AxesY.GetComponent<GenericAxisPrefab>()._swapped = _swappedY;
+        AxesY.SetActive(_activeY);
+        if (_flipY)
+            AxesY.transform.eulerAngles = new Vector3(180f, AxesY.transform.rotation.y, AxesY.transform.rotation.z);
+        
+        AxesZ = gameObject.transform.GetChild(2).gameObject;
+        AxesZ.GetComponent<GenericAxisPrefab>()._nameAxisLabel = _labelZ;
+        AxesZ.GetComponent<GenericAxisPrefab>()._swapped = _swappedZ;
+        AxesZ.SetActive(_activeZ);
+        if (_flipZ)
+            AxesZ.transform.eulerAngles = new Vector3(AxesZ.transform.rotation.z, AxesY.transform.rotation.y, 180f);
     }
 }
---
+++ Assets/Scripts/DataUtility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class DataUtility : MonoBehaviour
+{
+    JSONObject example;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        example = (JSONObject)JSON.Parse("{id:1, name: 'nuevo'}");
+        Debug.Log(example.ToString());
+        Debug.Log(example["name"]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
